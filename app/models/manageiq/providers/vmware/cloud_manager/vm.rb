3fe7005bfc16c10c543b7e896285a6645e2675a0
Vojko Rozic <vojko.rozic@xlab.si>
2018-02-13 12:16:31 +0100

    Added destroy vm functionality for vmware provider

--- app/models/manageiq/providers/vmware/cloud_manager/vm.rb
+++ app/models/manageiq/providers/vmware/cloud_manager/vm.rb
@@ -1,6 +1,8 @@
 class ManageIQ::Providers::Vmware::CloudManager::Vm < ManageIQ::Providers::CloudManager::Vm
   include_concern 'Operations'
 
+  before_destroy :raw_destroy
+
   def provider_object(connection = nil)
     connection ||= ext_management_system.connect
     connection.vms.get_single_vm(uid_ems)
@@ -21,5 +23,16 @@ class ManageIQ::Providers::Vmware::CloudManager::Vm < ManageIQ::Providers::Cloud
 
   def self.display_name(number = 1)
     n_('Instance (VMware vCloud)', 'Instances (VMware vCloud)', number)
+  end
+
+  def vm_destroy
+    with_provider_object(&:undeploy)
+
+    ext_management_system.with_provider_connection do |service|
+      service.delete_vapp(ems_ref)
+    end
+  rescue StandardError => err
+    $vcloud_log.error("stack=[#{name}], error: #{err}")
+    raise MiqException::MiqOrchestrationDeleteError, err.to_s, err.backtrace
   end
 end
--- spec/models/manageiq/providers/vmware/cloud_manager/vm_spec.rb
+++ spec/models/manageiq/providers/vmware/cloud_manager/vm_spec.rb
@@ -35,4 +35,22 @@ describe ManageIQ::Providers::Vmware::CloudManager::Vm do
       include_examples "Vm operation is not available"
     end
   end
+
+  context "when destroyed" do
+    let(:ems) { FactoryGirl.create(:ems_vmware_cloud) }
+    let(:vm) { FactoryGirl.create(:vm_vmware_cloud, :ext_management_system => ems) }
+    let(:connection) { double("connection", :delete_vapp => "delete_vapp") }
+    let(:provider_object) { double("provider_object", :undeploy => "undeploy") }
+
+    it "stop and delete virtual machine" do
+      allow(vm).to receive(:with_provider_object).and_yield(provider_object)
+      expect(ems).to receive(:with_provider_connection).and_yield(connection)
+
+      expect(provider_object).to receive(:undeploy)
+      expect(connection).to receive(:delete_vapp)
+
+      vm.vm_destroy
+    end
+  end
+
 end
